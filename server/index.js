d22d7d4021edd3f7f9c52eae28f9371c61744b9a
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-03-05 20:34:30 -0300

    prueba

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "./config.js";
 import indexRoutes from "../server/routes/index.routes.js";
 import taskRouter from "../server/routes/tasks.routes.js";
+import { PORT } from "./config.js";
 
 const app = express();
 
@@ -11,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(indexRoutes);
-app.use(taskRouter);
-app.listen(process.env.PORT || 4000);
-
-console.log("Server is running on port", process.env.PORT || 4000);
+app.use("/api", taskRouter);
+app.listen(PORT);
+console.log("Server running on port", PORT);
