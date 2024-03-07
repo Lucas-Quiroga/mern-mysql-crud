62382e5f1badcb2263a0a2fa636f9e830873f834
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-03-07 17:57:55 -0300

    prueba

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getTasksRequest = async () => {
   try {
     const response = await axios.get(
-      `${import.meta.env.VITE_API_URL}/api/tasks`
+      `${import.meta.env.VITE_API_URL}api/tasks`
     );
     return response; // Add this line to return the response data
   } catch (error) {
@@ -13,30 +13,30 @@ export const getTasksRequest = async () => {
 };
 
 export const createTaskRequest = async (task) => {
-  await axios.post(`${import.meta.env.VITE_API_URL}/api/tasks`, task);
+  await axios.post(`${import.meta.env.VITE_API_URL}api/tasks`, task);
 };
 
 export const deleteTaskRequest = async (id) => {
-  await axios.delete(`${import.meta.env.VITE_API_URL}/api/tasks/${id}`);
+  await axios.delete(`${import.meta.env.VITE_API_URL}api/tasks/${id}`);
 };
 
 export const getTaskRequest = async (id) => {
   const response = await axios.get(
-    `${import.meta.env.VITE_API_URL}/api/tasks/${id}`
+    `${import.meta.env.VITE_API_URL}api/tasks/${id}`
   );
   return response;
 };
 
 export const updateTaskRequest = async (id, newFields) => {
   const response = await axios.put(
-    `${import.meta.env.VITE_API_URL}/api/tasks/${id}`,
+    `${import.meta.env.VITE_API_URL}api/tasks/${id}`,
     newFields
   );
   return response;
 };
 
 export const toggleTaskDoneRequest = async (id, done) => {
-  await axios.put(`${import.meta.env.VITE_API_URL}/api/tasks/${id}`, {
+  await axios.put(`${import.meta.env.VITE_API_URL}api/tasks/${id}`, {
     done,
   });
 };
