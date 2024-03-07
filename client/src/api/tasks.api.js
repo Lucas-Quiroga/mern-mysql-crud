52aa5434a023e474d8bdb6cc98b5c606a69d98cd
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-03-07 18:09:52 -0300

    prueba

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -3,7 +3,12 @@ import axios from "axios";
 export const getTasksRequest = async () => {
   try {
     const response = await axios.get(
-      `${import.meta.env.VITE_API_URL}api/tasks`
+      `${import.meta.env.VITE_API_URL}api/tasks`,
+      {
+        headers: {
+          Origin: "https://mern-mysql.netlify.app",
+        },
+      }
     );
     return response; // Add this line to return the response data
   } catch (error) {
@@ -13,16 +18,29 @@ export const getTasksRequest = async () => {
 };
 
 export const createTaskRequest = async (task) => {
-  await axios.post(`${import.meta.env.VITE_API_URL}api/tasks`, task);
+  await axios.post(`${import.meta.env.VITE_API_URL}api/tasks`, task, {
+    headers: {
+      Origin: "https://mern-mysql.netlify.app",
+    },
+  });
 };
 
 export const deleteTaskRequest = async (id) => {
-  await axios.delete(`${import.meta.env.VITE_API_URL}api/tasks/${id}`);
+  await axios.delete(`${import.meta.env.VITE_API_URL}api/tasks/${id}`, {
+    headers: {
+      Origin: "https://mern-mysql.netlify.app",
+    },
+  });
 };
 
 export const getTaskRequest = async (id) => {
   const response = await axios.get(
-    `${import.meta.env.VITE_API_URL}api/tasks/${id}`
+    `${import.meta.env.VITE_API_URL}api/tasks/${id}`,
+    {
+      headers: {
+        Origin: "https://mern-mysql.netlify.app",
+      },
+    }
   );
   return response;
 };
@@ -30,13 +48,26 @@ export const getTaskRequest = async (id) => {
 export const updateTaskRequest = async (id, newFields) => {
   const response = await axios.put(
     `${import.meta.env.VITE_API_URL}api/tasks/${id}`,
-    newFields
+    newFields,
+    {
+      headers: {
+        Origin: "https://mern-mysql.netlify.app",
+      },
+    }
   );
   return response;
 };
 
 export const toggleTaskDoneRequest = async (id, done) => {
-  await axios.put(`${import.meta.env.VITE_API_URL}api/tasks/${id}`, {
-    done,
-  });
+  await axios.put(
+    `${import.meta.env.VITE_API_URL}api/tasks/${id}`,
+    {
+      done,
+    },
+    {
+      headers: {
+        Origin: "https://mern-mysql.netlify.app",
+      },
+    }
+  );
 };
